0b97435dbe062ab97a650abf70edbaebbf671eb8
Eukaris <eukaris870@gmail.com>
2017-07-20 15:34:00 -0400

    join lista de rutas asignadas

--- app/src/main/java/ve/gob/fundelec/simlec/ListaRutasAsignadas/entities/QueryRutas.java
+++ app/src/main/java/ve/gob/fundelec/simlec/ListaRutasAsignadas/entities/QueryRutas.java
@@ -39,6 +39,11 @@ public class QueryRutas {
     @Column
     String sector; // limite de 255
 
+    @Column
+    String cod_ruta; // limite de 10
+    @Column
+    String nom_ruta; // limite de 50
+
     public String getFch_programa() {
         return fch_programa;
     }
@@ -69,14 +74,6 @@ public class QueryRutas {
 
     public void setFch_asig_diaria(String fch_asig_diaria) {
         this.fch_asig_diaria = fch_asig_diaria;
-    }
-
-    public int getId_ruta() {
-        return id_ruta;
-    }
-
-    public void setId_ruta(int id_ruta) {
-        this.id_ruta = id_ruta;
     }
 
     public int getId_parroquia() {
@@ -117,5 +114,45 @@ public class QueryRutas {
 
     public void setSector(String sector) {
         this.sector = sector;
+    }
+
+    public int getId_programacion_calle() {
+        return id_programacion_calle;
+    }
+
+    public void setId_programacion_calle(int id_programacion_calle) {
+        this.id_programacion_calle = id_programacion_calle;
+    }
+
+    public int getId_calle_avenida() {
+        return id_calle_avenida;
+    }
+
+    public void setId_calle_avenida(int id_calle_avenida) {
+        this.id_calle_avenida = id_calle_avenida;
+    }
+
+    public String getCod_ruta() {
+        return cod_ruta;
+    }
+
+    public void setCod_ruta(String cod_ruta) {
+        this.cod_ruta = cod_ruta;
+    }
+
+    public String getNom_ruta() {
+        return nom_ruta;
+    }
+
+    public void setNom_ruta(String nom_ruta) {
+        this.nom_ruta = nom_ruta;
+    }
+
+    public int getId_ruta() {
+        return id_ruta;
+    }
+
+    public void setId_ruta(int id_ruta_tabla) {
+        this.id_ruta = id_ruta_tabla;
     }
 }
