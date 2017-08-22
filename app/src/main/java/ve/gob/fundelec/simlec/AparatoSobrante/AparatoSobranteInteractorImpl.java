c923a70b8e74da77d214e4cd3789691bbdb8d923
Eukaris <eukaris870@gmail.com>
2017-08-22 09:53:32 -0400

    archivos faltantes

---
+++ app/src/main/java/ve/gob/fundelec/simlec/AparatoSobrante/AparatoSobranteInteractorImpl.java
@@ -0,0 +1,11 @@
+package ve.gob.fundelec.simlec.AparatoSobrante;
+
+/**
+ * Created by fundelec on 22/08/17.
+ */
+
+public class AparatoSobranteInteractorImpl implements AparatoSobranteInteractor{
+
+
+
+}
---
+++ app/src/main/java/ve/gob/fundelec/simlec/AparatoSobrante/AparatoSobranteRepositoryImpl.java
@@ -0,0 +1,8 @@
+package ve.gob.fundelec.simlec.AparatoSobrante;
+
+/**
+ * Created by fundelec on 22/08/17.
+ */
+
+public class AparatoSobranteRepositoryImpl implements AparatoSobranteRepository {
+}
